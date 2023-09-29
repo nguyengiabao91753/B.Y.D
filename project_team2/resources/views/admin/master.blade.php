357670a17a06dfa423caa6afa88545e171a9f32b
Hoangduy2982003 <125873702+Hoangduy2982003@users.noreply.github.com>
2023-09-30 03:16:20 +0700

    lay fiel

--- project_team2/resources/views/admin/master.blade.php
+++ project_team2/resources/views/admin/master.blade.php
@@ -20,19 +20,20 @@
 
     <!-- Main content -->
     <section class="content">
+      @if ($errors->any())
         <div class="alert alert-danger alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
             <h5><i class="icon fas fa-ban"></i> Alert!</h5>
-            Danger alert preview. This alert is dismissable. A wonderful serenity has taken possession of my
-            entire
-            soul, like these sweet mornings of spring which I enjoy with my whole heart.
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
         </div>
-
-        <div class="alert alert-success alert-dismissible">
+      @endif
+        <!-- <div class="alert alert-success alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
             <h5><i class="icon fas fa-check"></i> Alert!</h5>
             Success alert preview. This alert is dismissable.
-        </div>
+        </div> -->
         
         @yield('content')
     </section>
